9c364338b0c76b75f4a5bd187561fb3dc0689fa3
NIKITA BAKHTAR <nikibakhtar@gmail.com>
2024-01-19 05:49:10 +0100

    Added test cases and code for nullexception and minus values

--- src/DebtCollect/DebtPaybackCal.java
+++ src/DebtCollect/DebtPaybackCal.java
@@ -6,7 +6,7 @@ public class DebtPaybackCal {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		
 		Scanner sc  = new Scanner(System.in);
 		System.out.println("Enter Debt Value");
 		double db = sc.nextDouble();
@@ -17,29 +17,45 @@ public class DebtPaybackCal {
 		System.out.println("Debt: "+db);
 		System.out.println("Interest rate: "+intrst);
 		System.out.println("Duration: "+dbdur);
+	
+		sc.close();
+
+	       
 		
 		double monthlyRate = calculateMonthlyRate(db, intrst, dbdur);
         double overallDebt = calculateOverallDebt(db, intrst, dbdur);
 
         // Display the results
         System.out.println("\nLoan Details:");
-        System.out.println("Monthly Rate: " + monthlyRate + " euros");
+        System.out.println("Monthly Payback Rate: " + monthlyRate + " euros");
         System.out.println("Number of Rates: " + dbdur);
-        System.out.println("Overall Debt: " + overallDebt + " euros");		
+        System.out.println("Overall Debt: " + overallDebt + " euros");
+        
+		
+			
 
 	}
 	public static double calculateMonthlyintrst(double intrst) {
+		if (intrst <= 0) {
+            return 0.0;
+        }
         return (intrst / 12 ) *  100;
     }
 
     // Function to calculate the monthly rate
     public static double calculateMonthlyRate(double db, double intrst, int dbdur) {
+    	 if (db <= 0 || intrst <= 0 || dbdur <= 0) {
+             return 0.0;
+         }
         double monthlyintrst = calculateMonthlyintrst(intrst);
         return (db / dbdur) + monthlyintrst;
     }
 
     // Function to calculate the overall debt
     public static double calculateOverallDebt(double db, double intrst, int numberOfRates) {
+    	 if (db <= 0 || intrst <= 0 || numberOfRates <= 0) {
+             return 0.0;
+         }
     	 double monthly = calculateMonthlyRate( db,  intrst, numberOfRates);
     	return  (monthly * numberOfRates);
     }
--- src/DebtCollect/DebtPaybackCalTest.java
+++ src/DebtCollect/DebtPaybackCalTest.java
@@ -10,13 +10,16 @@ public class DebtPaybackCalTest {
 	public void testMonthlyRate() 
 	{
 		assertEquals(875.0, DebtPaybackCal.calculateMonthlyRate(10000, 5, 12));
+		assertEquals(0.0, DebtPaybackCal.calculateMonthlyRate(0.0, 0.0, 0));
+		assertEquals(0.0, DebtPaybackCal.calculateMonthlyRate(-444.0, 5.0, 0));
 		
 	}
 	@Test
 	public void testOverallDebt() 
 	{
 		assertEquals(10500.0, DebtPaybackCal.calculateOverallDebt(10000, 5, 12));
-		
+		assertEquals(0.0, DebtPaybackCal.calculateOverallDebt(0.0, 0.0, 0));
+		assertEquals(0.0, DebtPaybackCal.calculateOverallDebt(-444.0, 5.0, 0));
 	}
 
 }
